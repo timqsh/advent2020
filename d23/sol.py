e38152cce6002e102029dde81493716e2e81eb33
Timur Kushukov <timqsh@gmail.com>
2020-12-24 02:07:59 +0500

    day 23 refactor

--- d23/sol.py
+++ d23/sol.py
@@ -1,50 +1,9 @@
-from typing import List, Tuple
-
 import numpy as np
 from numba import njit
 
 
-# @njit
-def move(digs: List[int], cur_idx: int) -> Tuple[List[int], int]:
-    lowest = 1  # min(digs)
-    highest = len(digs)  # max(digs)
-    three = (
-        digs[cur_idx + 1 : cur_idx + 4]
-        + digs[: cur_idx - len(digs) + 4 if cur_idx - len(digs) + 4 >= 0 else 0]
-    )
-    cur = digs[cur_idx]
-    dest = digs[cur_idx] - 1
-    while True:
-        if dest < lowest:
-            dest = highest
-        if dest not in three:
-            break
-        dest -= 1
-    new_digs = (
-        digs[
-            cur_idx - len(digs) + 4 if cur_idx - len(digs) + 4 >= 0 else 0 : cur_idx + 1
-        ]
-        + digs[cur_idx + 4 :]
-    )
-    dest_idx = new_digs.index(dest)
-    for elem in three[::-1]:
-        new_digs.insert(dest_idx + 1, elem)
-    new_idx = (new_digs.index(cur) + 1) % len(digs)
-    return new_digs, new_idx
-
-
-def solve(txt: str, moves: int) -> str:
-    digs = [int(i) for i in txt]
-    cur_idx = 0
-    for _ in range(moves):
-        digs, cur_idx = move(digs, cur_idx)
-
-    one_idx = digs.index(1)
-    return "".join(str(d) for d in digs[one_idx + 1 :] + digs[:one_idx])
-
-
 @njit(cache=True)
-def move_2(arr: np.ndarray) -> None:
+def move(arr: np.ndarray) -> None:
     lowest = 1
     highest = len(arr) - 1
     cur = arr[0]
@@ -66,21 +25,21 @@ def move_2(arr: np.ndarray) -> None:
 
 
 @njit(cache=True)
-def move_2_n(arr: np.ndarray, n) -> None:
+def move_n(arr: np.ndarray, n) -> None:
     for i in range(n):
         if i % 10 ** 6 == 0:
             print("iteration #" + str(i))
-        move_2(arr)
+        move(arr)
 
 
-def solve_2(txt: str, moves: int) -> str:
+def part_1(txt: str, moves: int) -> str:
     digs = [int(i) for i in txt]
     arr = np.zeros(max(digs) + 1, dtype=int)
     arr[0] = digs[0]  # pointer in first elem
     for i in range(len(digs)):
         arr[digs[i]] = digs[(i + 1) % len(digs)]
     for _ in range(moves):
-        move_2(arr)
+        move(arr)
 
     start = arr[1]
     result = []
@@ -100,20 +59,16 @@ def part_2(txt: str) -> int:
     for i in range(len(digs) + 1, 10 ** 6):
         arr[i] = i + 1
     arr[10 ** 6] = digs[0]
-    move_2_n(arr, 10 ** 7)
+    move_n(arr, 10 ** 7)
 
     f1 = arr[1]
     f2 = arr[f1]
     return f1 * f2
 
 
-assert solve("389125467", 10) == "92658374"
-assert solve("389125467", 100) == "67384529"
-result_1 = solve("364289715", 100)
-print(f"part 1: {result_1}")
-assert result_1 == "98645732"
+if __name__ == "__main__":
+    result_1 = part_1("364289715", 100)
+    print(f"part 1: {result_1}")
 
-assert part_2("389125467") == 149245887792
-result_2 = part_2("364289715")
-print(f"part 2: {result_2}")
-assert result_2 == 689500518476
+    result_2 = part_2("364289715")
+    print(f"part 2: {result_2}")
---
+++ d23/test_d23.py
@@ -0,0 +1,14 @@
+from d23.sol import part_1, part_2
+
+
+def test_part_1():
+    assert part_1("389125467", 10) == "92658374"
+    assert part_1("389125467", 100) == "67384529"
+    result_1 = part_1("364289715", 100)
+    assert result_1 == "98645732"
+
+
+def test_part_2():
+    assert part_2("389125467") == 149245887792
+    result_2 = part_2("364289715")
+    assert result_2 == 689500518476
